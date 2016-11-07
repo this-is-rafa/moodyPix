5678155c340050fab62b3206856eeff7bef2a266
this-is-rafa <rafa@ratradio.net>
2016-11-07 17:48:05 -0500

    add styling to swatches, swatch box, hover event

--- app/assets/javascripts/pictures.js
+++ app/assets/javascripts/pictures.js
@@ -2,21 +2,36 @@
 // All this logic will automatically be available in application.js.
 $(document).on('turbolinks:load', function() {
   
-  $('.label-single-color').first(function() {
-    var cssColor = $(this).css('background-color');
-    var colorHTML = "<p>color: " + cssColor + ";</p><p>background-color: " + cssColor + ";</p>";
-    $('.css-colors-code').html(colorHTML);
-  });
+  var firstColor = $('.label-single-color').first();
+  var cssColor = $(firstColor).css('background-color');
+  var colorHTML = "<div class='swatch' style='background-color:" + cssColor + "'></div><p>color: " + cssColor + ";</p><p>background-color: " + cssColor + ";</p>";
+  $('.css-colors-code').html(colorHTML);
+
+
 
   $('.label-title > a').click(function(){
     $(this).children('.fa-caret-right').toggleClass('open');
   });
+
+
+
   //Show the color code in the box when you click the color.
   $('.label-single-color').click(function() {
     var cssColor = $(this).css('background-color');
-    var colorHTML = "<p>color: " + cssColor + ";</p><p>background-color: " + cssColor + ";</p>";
+    var colorHTML = "<div class='swatch' style='background-color:" + cssColor + "'></div><p>color: " + cssColor + ";</p><p>background-color: " + cssColor + ";</p>";
     $('.css-colors-code').html(colorHTML);
   });
 
+  //   $('.label-single-color').hover(function() {
+  //   var cssColor = $(this).css('background-color');
+  //   var colorHTML = "<div class='swatch' style='background-color:" + cssColor + "'></div><p>color: " + cssColor + ";</p><p>background-color: " + cssColor + ";</p>";
+  //   $('.css-colors-code').html(colorHTML);
+  // });
   
+  $('.label-single-color').hover(function() {
+    var cssColor = $(this).css('background-color');
+    $('#site-title > a').css('color', cssColor);
+  });
+
+
 });
