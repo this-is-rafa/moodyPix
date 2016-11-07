7cff2ee9f2a56fb289075e63b6f198d925df6900
this-is-rafa <rafa@ratradio.net>
2016-11-07 12:53:10 -0500

    start of new show view

--- app/assets/javascripts/pictures.js
+++ app/assets/javascripts/pictures.js
@@ -7,10 +7,6 @@ $(document).on('turbolinks:load', function() {
     var colorHTML = "<p>color: " + cssColor + ";</p><p>background-color: " + cssColor + ";</p>";
     $('.css-colors-code').html(colorHTML);
   });
-  //expand the image labels
-  // $('.img-labels').click(function (){
-  //   $(this).toggleClass('label-expand');
-  // });
 
   //Show the color code in the box when you click the color.
   $('.label-single-color').click(function() {
