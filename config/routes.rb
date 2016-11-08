31b67c23d1ac663a20c155d22a941c82775a9d1f
David Collado <dmcollado@gmail.com>
2016-11-07 19:20:41 -0500

    Adding URL submission for on all views + further styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root "pictures#index"
+  root 'pictures#index'
+  get '/pictures', to: 'pictures#gallery'
   resources :reviews
   resources :pictures
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
