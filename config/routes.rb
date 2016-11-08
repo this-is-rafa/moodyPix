41e4306b1b6bfc53e859e9232e4750c1c8333a24
David Collado <dmcollado@gmail.com>
2016-11-07 20:58:21 -0500

    Adding delete button to gallery tiles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+
+  delete '/pictures/:id' => 'pictures#destroy'
+
+  delete '/pictures' do
+
+  end
+
   root 'pictures#index'
+
   get '/pictures', to: 'pictures#gallery'
+
   resources :reviews
   resources :pictures
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
