ec09686a8f538e9b357eeb1885adcf467325ada6
this-is-rafa <rafa@ratradio.net>
2016-11-14 16:50:16 -0500

    add hex to js

--- app/assets/javascripts/pictures.js
+++ app/assets/javascripts/pictures.js
@@ -1,37 +1,45 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 $(document).on('turbolinks:load', function() {
-  
-  var firstColor = $('.label-single-color').first();
-  var cssColor = $(firstColor).css('background-color');
-  var colorHTML = "<div class='swatch' style='background-color:" + cssColor + "'></div><p>color: " + cssColor + ";</p><p>background-color: " + cssColor + ";</p>";
-  $('.css-colors-code').html(colorHTML);
 
+// Turn RGB Values to Hex
+function rgb2hex(rgb){
+ rgb = rgb.match(/^rgba?[\s+]?\([\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?,[\s+]?(\d+)[\s+]?/i);
+ return (rgb && rgb.length === 4) ? "#" +
+  ("0" + parseInt(rgb[1],10).toString(16)).slice(-2) +
+  ("0" + parseInt(rgb[2],10).toString(16)).slice(-2) +
+  ("0" + parseInt(rgb[3],10).toString(16)).slice(-2) : '';
+}
 
-
+// Turn the carot when the div expands
   $('.label-title > a').click(function(){
     $(this).children('.fa-caret-right').toggleClass('open');
   });
 
 
 
-  //Show the color code in the box when you click the color.
+// Set initial color codes on load
+  var firstColor = $('.label-single-color').first();
+  var cssColor = $(firstColor).css('background-color');
+  var hexColor = rgb2hex(cssColor);
+    var colorHTML = "<div class='swatch' style='background-color:" + cssColor + "'></div><p>/* Hex */ color: " + hexColor + ";</p><p>/* RGB */ color: " + cssColor + "<p></p><p>/* Hex */ background-color: " + hexColor + ";</p><p>/* RGB */ background-color: " + cssColor + ";</p>";
+  $('.css-colors-code').html(colorHTML);
+
+
+//Change the color code in the box when you click the color.
   $('.label-single-color').click(function() {
     var cssColor = $(this).css('background-color');
-    var colorHTML = "<div class='swatch' style='background-color:" + cssColor + "'></div><p>color: " + cssColor + ";</p><p>background-color: " + cssColor + ";</p>";
+    var hexColor = rgb2hex(cssColor);
+    var colorHTML = "<div class='swatch' style='background-color:" + cssColor + "'></div><p>/* Hex */ color: " + hexColor + ";</p><p>/* RGB */ color: " + cssColor + "<p></p><p>/* Hex */ background-color: " + hexColor + ";</p><p>/* RGB */ background-color: " + cssColor + ";</p>";
     $('.css-colors-code').html(colorHTML);
   });
 
-  //   $('.label-single-color').hover(function() {
-  //   var cssColor = $(this).css('background-color');
-  //   var colorHTML = "<div class='swatch' style='background-color:" + cssColor + "'></div><p>color: " + cssColor + ";</p><p>background-color: " + cssColor + ";</p>";
-  //   $('.css-colors-code').html(colorHTML);
-  // });
-  
+
+// Change the header title on hover
   $('.label-single-color').hover(function() {
     var cssColor = $(this).css('background-color');
     $('#site-title > a').css('color', cssColor);
   });
 
-
 });
+
